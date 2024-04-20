10f2bc3a14eee1abeac9de0d5c404567be7fef87
taroj1205 <taroj1205@gmail.com>
2024-04-20 20:37:27 +1200

    feat: basic astro version of portfolio release

---
+++ src/i18n/ui.ts
@@ -0,0 +1,14 @@
+import { translations as translationsEN } from "../locales/en/translation"
+import { translations as translationsJA } from "../locales/ja/translation"
+
+export const languages = {
+  en: 'English',
+  ja: '日本語',
+}
+
+export const defaultLang = 'en'
+
+export const ui = {
+  en: translationsEN,
+  ja: translationsJA,
+} as const
