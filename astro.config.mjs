28ff112ca78c3d3452047b237a3025f7080ff5b1
taroj1205 <taroj1205@gmail.com>
2024-04-23 17:26:13 +1200

    chore: Update npm dependencies and add lazy loading for images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from "astro/config"
 
+import singleFile from "astro-single-file"
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  site: "https://astro-taroj.vercel.app",
+  integrations: [singleFile()],
+})
