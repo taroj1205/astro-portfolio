ec684379fd7c16110f1928b7c95800992375fb38
taroj1205 <taroj1205@gmail.com>
2024-04-23 21:21:23 +1200

    chore: Add theme color meta tag to layout and update Astro configuration for PWA integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config"
-
 import sitemap from "@astrojs/sitemap"
+import AstroPWA from "@vite-pwa/astro"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://taroj1205.poyo.jp",
   output: "static",
-  integrations: [sitemap()],
+  integrations: [sitemap(), AstroPWA()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ja"],
