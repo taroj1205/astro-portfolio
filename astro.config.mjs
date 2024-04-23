524d49f8bbe87a675da9d44d04e559a605b917fe
taroj1205 <taroj1205@gmail.com>
2024-04-23 19:29:16 +1200

    chore: Remove astro-single-file integration and html-minifier-terser dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config"
 
-import singleFile from "astro-single-file"
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-taroj.vercel.app",
-  integrations: [singleFile()],
 })
