fa1c3b5521dc6266ff1e794a8bce98a66cc0b5f0
taroj1205 <taroj1205@gmail.com>
2024-04-23 19:47:59 +1200

    chore: Update Astro configuration for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config"
-import vercelStatic from "@astrojs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-taroj.vercel.app",
-  output: "static",
-  adapter: vercelStatic(),
 })
